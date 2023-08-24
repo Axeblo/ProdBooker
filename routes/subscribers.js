cb262a7dc7cc7215de9299e0394f118b2576d43c
Axel Blomkvist <axelblomkvist@gmail.com@>
2023-08-24 02:41:50 +0200

    REST API test

---
+++ routes/subscribers.js
@@ -0,0 +1,28 @@
+import express from "express"
+import Subscriber from "../models/subscriber.js"
+
+const router = express.Router()
+
+router.get('/', async (req,res) => {
+    //res.send("hello")
+    try {
+        const subscribers = await Subscriber.find();
+        res.json(subscribers);
+    } catch(err) {
+        res.status(500).json({message: err.message});
+    }
+})
+router.get('/:id', (req,res) => {
+    res.send(req.params.id);
+})
+router.post('/', (req,res) => {
+
+})
+router.patch('/:id', (req,res) => {
+
+})
+router.delete('/:id', (req,res) => {
+
+})
+
+export default router;
--- server.js
+++ server.js
@@ -1,57 +1,29 @@
-import { MongoClient } from "mongodb";
+import 'dotenv/config'
+import mongoose from "mongoose";
 import express from 'express';
+import subscribersRouter from "./routes/subscribers.js";
 
 const PORT = 3000;
 const app = express();
 
-const MONGO_URL = "mongodb://root:example@localhost:27017/";
 const DB_NAME = 'new_database';
 const COLLECTION_NAME = 'test_collection';
 
-
-let db, collection;
-
-MongoClient.connect(MONGO_URL, { useUnifiedTopology: true }, (err, client) => {
-  if (err) {
-    console.error('Failed to connect to MongoDB', err);
-    return;
-  }
-
-  console.log('Connected to MongoDB');
-  db = client.db(DB_NAME);
-  collection = db.collection(COLLECTION_NAME);
+//mongoose.connect("mongodb://localhost:27017/");
+mongoose.connect(process.env.MONGO_URL, {
+    user: "root",
+    pass: "example",
 });
 
-// Define the endpoint
-app.get('/hello', (req, res) => {
-    
-    if (!db) {
-        res.status(500).send({ error: 'Database not initialized, no db' });
-        return;
-    }
+const db = mongoose.connection;
+db.on("error", (error) => console.log(error) )
+db.once("open", () => console.log("Connected to database") )
 
-    if (!collection) {
-        res.status(500).send({ error: 'Database not initialized, no collection' });
-        return;
-    }
+app.use(express.json());
 
-    res.send('hello');
-    
-    const message = { string: 'hello!' };
+app.use('/subscribers', subscribersRouter)
 
-    collection.insertOne(message, (err, result) => {
-        if (err) {
-        res.status(500).send({ error: 'Failed to insert record.' });
-        return;
-        }
-
-        res.status(201).send({ message: 'Record added successfully!', record: result.ops[0] });
-    });
-
-    
-});
-
-// Start the server
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log("Server has started!");
 });
+
